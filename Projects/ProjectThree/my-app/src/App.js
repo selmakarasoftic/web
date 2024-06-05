dbe3116df3edab0f4ed17ab410248beb87948032
Selma Karasoftic <selma.karasoftic@stu.ibu.edu.ba>
2024-06-05 12:12:17 +0200

    new vers

--- Projects/ProjectThree/my-app/src/App.js
+++ Projects/ProjectThree/my-app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home.jsx";
+import Navbar from './Components/Navbar';
+import Contact from './Pages/Contact'
 
 function App() {
   return (
     <Router>
-      <Navbar /> // this is how the components are being called
+      <Navbar /> 
       <div className="container">
         <Routes>
           <Route path="/contact" element={<Contact />} />
